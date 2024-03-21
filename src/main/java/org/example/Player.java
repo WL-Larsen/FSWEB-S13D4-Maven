062598e221ae7490c27cb5e3d380fcb0045aa4d0
Huseyin-Celebi <hcelebi057@gmail.com>
2024-03-21 16:53:18 +0300

    solution

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Player {
+    private String name;
+    private int healthPercentage;
+    private final Weapon weapon;
+
+    public Player(String name, int healthPercentage, Weapon weapon) {
+        this.name = name;
+        this.healthPercentage =Math.min(Math.max(healthPercentage, 0), 100);
+        this.weapon = weapon;
+    }
+
+    public int healthRemaining() {
+        return healthPercentage;
+    }
+
+    public void loseHealth(int damage) {
+        healthPercentage -= damage;
+        if (healthPercentage <= 0) {
+            System.out.println(name + " player has been knocked out of game");
+        }
+    }
+
+    public void restoreHealth(int healthPotion) {
+        healthPercentage = Math.min(100, healthPercentage + healthPotion);
+    }
+}
